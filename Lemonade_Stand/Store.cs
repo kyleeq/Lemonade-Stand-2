6fb1bdf225029415aaee5b17ccae11db97138b1a
unknown <kylee.schreibs2@yahoo.com>
2019-04-01 14:59:15 -0500

    add all objects to UML diagram

---
+++ Lemonade_Stand/Store.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lemonade_Stand
+{
+    public class Store
+    {
+        public int lemons
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int sugar
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int ice
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+
+        public int paperCups
+        {
+            get => default(int);
+            set
+            {
+            }
+        }
+    }
+}
