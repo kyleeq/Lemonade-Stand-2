03617c6665be849084604e7c3882a6a8313a7b87
unknown <kylee.schreibs2@yahoo.com>
2019-04-02 17:01:27 -0500

    add variables to Day class

--- Lemonade_Stand/Day.cs
+++ Lemonade_Stand/Day.cs
@@ -7,8 +7,16 @@ namespace Lemonade_Stand
 {
     public class Day
     {
+        // member variables
+        private int lemonsUsed;
+        private int cupsUsed;
+        private int sugarUsed;
+        private int iceReset;
         private int customerList;
 
+        // constructor
+
+        //member methods
         public Weather Weather
         {
             get => default(Weather);
