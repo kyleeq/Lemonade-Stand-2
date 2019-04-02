60bdb5edcf5bb0d9d8a3692f149dfb9a2cc5baba
unknown <kylee.schreibs2@yahoo.com>
2019-04-02 16:25:32 -0500

    rename GetRecipe to SetRecipe and edit it AND make RecipePrompt in UserInterface class

--- Lemonade_Stand/Recipe.cs
+++ Lemonade_Stand/Recipe.cs
@@ -10,21 +10,19 @@ namespace Lemonade_Stand
         // member variables
         private int lemonsPerPitcher;
         private int sugerInCupsPerPitcher;
-        private int icePerCup;
+        private int iceCubesPerCup;
 
         // constructor
 
         // member methods
-        public void GetRecipe()
+        public void SetRecipe(int lemonsPerPitcher, int sugarInCupsPerPitcher, int iceCubesPerCup)
         {
-            Console.WriteLine("Would you like to enter in your own recipe or use the generic recipe? Please type in 'my own' or 'generic'. " +
-                "The generic recipe consists of four lemons and four cups of sugar per pitcher and four ice cubes per glass of lemonade");
             Console.WriteLine("Enter the amount of lemons you would like per pitcher of lemonade");
-            int lemonsPerPitcher = Convert.ToInt32(Console.ReadLine());
+            lemonsPerPitcher = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter the amount of sugar, in cups, you would like per pitcher of lemonade");
-            int sugarInCUpsPerPitcher = Convert.ToInt32(Console.ReadLine());
+            sugarInCupsPerPitcher = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter how many ice cubes you will be putting into each glass of lemonade");
-            int icePerCup = Convert.ToInt32(Console.ReadLine());
+            iceCubesPerCup = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
