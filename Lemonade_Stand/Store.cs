59bd5d8ee4f76b0ed6d35959f33db895750a7f73
unknown <kylee.schreibs2@yahoo.com>
2019-04-03 10:45:04 -0500

    move GoToStore function and itemsBought variables into Store class

--- Lemonade_Stand/Store.cs
+++ Lemonade_Stand/Store.cs
@@ -8,17 +8,30 @@ namespace Lemonade_Stand
     public class Store
     {
         // member variables
-        private int paperCups;
-        private int icePrice;
-        private int cupsPrice;
         private int lemonPrice;
+        private int sugarPrice;                        
+        private int paperCupsPrice;
+        private int icePrice;
+
+        private int lemonsBought; // on a given day
+        private int sugarBought;
+        private int cupsBought;
+        private int iceCubesBought;
 
         // constructor
 
         // member methods
-        public int GoToStore()
+        public void GoToStore()
         {
-
+            Console.WriteLine("Let's go to the store and purchase some ingrediants for your lemonade.");
+            Console.WriteLine("Enter how many lemons would you like to buy");
+            lemonsBought = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter how many cups of sugar you would like to buy");
+            sugarBought = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter how many paper cups you would like to buy");
+            cupsBought = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter how many ice cubs you would like to buy");
+            iceCubesBought = Convert.ToInt32(Console.ReadLine());
         }
     }
 
