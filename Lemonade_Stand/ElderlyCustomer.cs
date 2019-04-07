f4771102829c5fd77119855251b4de7639719155
unknown <kylee.schreibs2@yahoo.com>
2019-04-07 18:50:28 -0500

    add and edit conditionals to MiddleAged and Elderly Customer classes

--- Lemonade_Stand/ElderlyCustomer.cs
+++ Lemonade_Stand/ElderlyCustomer.cs
@@ -18,7 +18,26 @@ namespace Lemonade_Stand
         // member methods
         public override void PricePointDeterminant(double lemonadePrice)
         {
-
+            if (lemonadePrice < 0.25)
+            {
+                for (double i = 0; i <= 25; i++);
+            }
+            else if (lemonadePrice >= 0.25 && lemonadePrice < 0.50)
+            {
+                for (double i = 0; i <= 25; i += 2);
+            }
+            else if (lemonadePrice <= 0.50 && lemonadePrice < 0.75)
+            {
+                for (double i = 0; i <= 25; i += 3);
+            }
+            else if (lemonadePrice <= 0.75 && lemonadePrice < 1.00)
+            {
+                for (double i = 0; i <= 25; i += 5);
+            }
+            else
+            {
+                for (double i = 0; i <= 25; i += 8);
+            }
         }
     }
 }
--- Lemonade_Stand/MiddleAgedCustomer.cs
+++ Lemonade_Stand/MiddleAgedCustomer.cs
@@ -18,7 +18,26 @@ namespace Lemonade_Stand
         // member methods
         public override void PricePointDeterminant(double lemonadePrice)
         {
-
+            if (lemonadePrice < 0.25)
+            {
+                for (double i = 0; i <= 25; i++);
+            }
+            else if (lemonadePrice >= 0.25 && lemonadePrice < 0.50)
+            {
+                for (double i = 0; i <= 25; i += 2);
+            }
+            else if (lemonadePrice <= 0.50 && lemonadePrice < 0.75)
+            {
+                for (double i = 0; i <= 25; i += 4);
+            }
+            else if (lemonadePrice <= 0.75 && lemonadePrice < 1.00)
+            {
+                for (double i = 0; i <= 25; i += 6);
+            }
+            else
+            {
+                for (double i = 0; i <= 25; i += 7);
+            }
         }
     }
 }
