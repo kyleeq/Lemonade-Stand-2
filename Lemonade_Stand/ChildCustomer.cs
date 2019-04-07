8e58e1ffdf9c9a0ecec47e0a31eb6ca7a253a03f
unknown <kylee.schreibs2@yahoo.com>
2019-04-06 20:58:08 -0500

    add PricePointDeterminant functionality to Child : Customer class

--- Lemonade_Stand/ChildCustomer.cs
+++ Lemonade_Stand/ChildCustomer.cs
@@ -5,7 +5,39 @@ using System.Text;
 
 namespace Lemonade_Stand
 {
-    public class ChildCustomer
+    public class Child : Customer
     {
+        // member variables
+
+
+
+        // constructor
+        public Child()
+        {          
+            
+        }
+
+        // member methods
+        public override void PricePointDeterminant(double lemonadePrice)
+        {
+            if (lemonadePrice < 0.25)
+            {
+                for (double i = 0; i <= 50; i += 2);
+            }
+            else if (lemonadePrice >= 0.25 && lemonadePrice < 0.50)
+            {
+                for (double i = 0; i <= 50; i += 4);
+            }
+            else if (lemonadePrice <= 0.50 && lemonadePrice < 0.75)
+            {
+                for (double i = 0; i <= 50; i += 7);
+            }
+            else
+            {
+                for (double i = 0; i <= 50; i += 10) ;
+            }
+
+                    
+        }
     }
 }
