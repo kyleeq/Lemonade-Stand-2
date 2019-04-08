eff3d6a95aea8146093b99734158fde014af11a2
unknown <kylee.schreibs2@yahoo.com>
2019-04-07 23:41:34 -0500

    add currentTemp conditionals to Child Customer class

--- Lemonade_Stand/ChildCustomer.cs
+++ Lemonade_Stand/ChildCustomer.cs
@@ -19,41 +19,62 @@ namespace Lemonade_Stand
         public override void PricePointDeterminant(double lemonadePrice, double currentTemp)
         {
             int customerPercent;
-
+            
             if (lemonadePrice < 0.25)
-            {                
+            {               
+                if (currentTemp < 75)
+                {
+                    willBuy = false;
+                }
                 willBuy = true;
             }
+
             else if (lemonadePrice >= 0.25 && lemonadePrice < 0.50)
             {
-                customerPercent = customerGenerator.Next(2);
-                if (customerPercent == 2)
+                if (currentTemp < 80)
                 {
-                    willBuy = true;
+                    willBuy = false; 
+                    customerPercent = customerGenerator.Next(2);
+                    if (customerPercent == 2)
+                    {
+                        willBuy = true;
+                    }
                 }
             }
             else if (lemonadePrice <= 0.50 && lemonadePrice < 0.75)
             {
-                customerPercent = customerGenerator.Next(5);
-                if (customerPercent == 5)
+                if (currentTemp < 85)
                 {
-                    willBuy = true;
+                    willBuy = false;
+                    customerPercent = customerGenerator.Next(5);
+                    if (customerPercent == 5)
+                    {
+                        willBuy = true;
+                    }
                 }
             }
             else if (lemonadePrice <= 0.75 && lemonadePrice < 1.00)
             {
-                customerPercent = customerGenerator.Next(6);
-                if (customerPercent == 6)
+                if (currentTemp < 90)
                 {
-                    willBuy = true;
+                    willBuy = false;
+                    customerPercent = customerGenerator.Next(6);
+                    if (customerPercent == 6)
+                    {
+                        willBuy = true;
+                    }
                 }
             }
             else
             {
-                customerPercent = customerGenerator.Next(9);
-                if (customerPercent == 9)
+                if (currentTemp < 95)
                 {
-                    willBuy = true;
+                    willBuy = false;
+                    customerPercent = customerGenerator.Next(7);
+                    if (customerPercent == 7)
+                    {
+                        willBuy = true;
+                    }
                 }
             }
             
